a121be886fcf1a541abda043ae78dcfae28a2951
Ananth Subramanya Mudigere <ananth@Ananths-MacBook-Air.local>
2024-09-24 22:37:16 +0530

    new

--- app/signin/[[...signin]]/page.tsx
+++ app/signin/[[...signin]]/page.tsx
@@ -4,7 +4,7 @@ export default function SignInPage() {
 	return (
 		<div className="relative h-screen w-full overflow-hidden bg-gray-900">
 		<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
-		<SignIn />
+		<SignIn signUpUrl={`${process.env.NEXT_PUBLIC_APP_URL}/signup`}/>
 		</div>
 		</div>
 	);
